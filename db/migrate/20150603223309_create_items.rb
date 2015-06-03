2c2d6e3f849e5692ae835ad08a32d5ba564be5e5
Nick Hong <nick@scalablepress.com>
2015-06-03 15:40:32 -0700

    Add user and item migration

--- db/migrate/20150603223309_create_items.rb
+++ db/migrate/20150603223309_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-
+      t.string :item_type
+      t.decimal :amount, precision: 8, scale: 2
+      t.datetime :date
+      t.integer :user_id
       t.timestamps null: false
     end
   end
