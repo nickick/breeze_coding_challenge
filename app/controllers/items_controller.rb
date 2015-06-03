85e83a48358e68edfda8c172663159019eee1d1a
Nick Hong <nick@scalablepress.com>
2015-06-03 16:33:44 -0700

    Add controller endpoint for creating items for users

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,17 @@
+class ItemsController < ApplicationController
+  protect_from_forgery except: :create
+
+  def create
+    @item = Item.create item_params.merge user_id: params[:user_id]
+
+    respond_to do |format|
+      format.json { render 'create.js' }
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_type, :amount, :date)
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  validates :amount, :item_type, :date, :user_id, presence: true
 end
